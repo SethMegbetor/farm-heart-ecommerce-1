2a65f5a1ce1f469ebc156dfe35ab0f7d9e0ca594
“SethMegbetor” <sethmegbetor@gmail.com>
2022-10-30 22:41:51 +0000

    tested CSS files and they are working

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,13 @@ const Home = () => {
   return (
     <>
       HeroBanner
-      <div>
+      <div className="products-heading">
         <h2>Fresh healthy vegetables</h2>
         <p>best organic farm products</p>
       </div>
-      <div>{["Product 1", "Product 2"].map((product) => product)}</div>
+      <div className="products-container">
+        {["Product 1", "Product 2"].map((product) => product)}
+      </div>
       Footer
     </>
   );
