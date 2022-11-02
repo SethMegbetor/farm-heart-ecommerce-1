b76ae2b0eca06d0b7457fd6517dd825358859a69
“SethMegbetor” <sethmegbetor@gmail.com>
2022-11-02 23:22:05 +0000

    solved product error

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import React from "react";
 import { client } from "../lib/client";
 import { Product, FooterBanner, HeroBanner } from "../components";
 
-const Home = (products, bannerData) => (
+const Home = ({ products, bannerData }) => (
   <>
     <HeroBanner heroBanner={bannerData.length & bannerData[0]} />
     <div className="products-heading">
