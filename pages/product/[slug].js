cbf4af46517e83ed2b56cb8ff1d1063aebd85ff2
“SethMegbetor” <sethmegbetor@gmail.com>
2022-11-07 13:53:29 +0000

    moving on to the slugs

---
+++ pages/product/[slug].js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProductDetails = () => {
+  return (
+    <div className="product-detail-container">
+      <div>
+        <div className="image-container">
+          <img src="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetails;
