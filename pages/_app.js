009f612c059392b9766f210a369ab48e9f92f6e2
“SethMegbetor” <sethmegbetor@gmail.com>
2022-11-08 19:31:43 +0000

    resolved the counting error

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,36 @@
-import React from "react";
+// import React from "react";
 
-import { Layout } from "../components";
-import { StateContext } from "../context/StateContext";
+// import { Layout } from "../components";
+// import { StateContext } from "../context/StateContext";
+// import { Toaster } from "react-hot-toast";
+
+// import "../styles/globals.css";
+
+// function MyApp({ Component, pageProps }) {
+//   return (
+//     <StateContext>
+//       <Layout>
+//         <Toaster/>
+//         <Component {...pageProps} />
+//       </Layout>
+//     </StateContext>
+//   );
+// }
+
+// export default MyApp;
+
+import React from "react";
 import { Toaster } from "react-hot-toast";
 
+import { Layout } from "../components";
 import "../styles/globals.css";
-
+import { StateContext } from "../context/StateContext";
 
 function MyApp({ Component, pageProps }) {
   return (
     <StateContext>
       <Layout>
-        <Toaster/>
+        <Toaster />
         <Component {...pageProps} />
       </Layout>
     </StateContext>
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,3 +1,135 @@
+// import React, { useState } from "react";
+// import {
+//   AiOutlineMinus,
+//   AiOutlinePlus,
+//   AiFillStar,
+//   AiOutlineStar,
+// } from "react-icons/ai";
+
+// import { client, urlFor } from "../../lib/client";
+// import { Product } from "../../components";
+
+// import { useStateContext } from "../../context/StateContext";
+// const ProductDetails = ({ product, products }) => {
+//   const { image, name, details, price } = product;
+
+//   const [index, setIndex] = useState(0);
+
+//   const { decQty, incQty, qty } = useStateContext();
+
+//   return (
+//     <div>
+//       <div className="product-detail-container">
+//         <div>
+//           <div className="image-container">
+//             <img
+//               src={urlFor(image && image[index])}
+//               className="product-detail-image"
+//             />
+//           </div>
+//           <div className="small-images-container">
+//             {image?.map((item, i) => (
+//               <img
+//                 src={urlFor(item)}
+//                 className={
+//                   i === index ? "small-image selected-image" : "small-image"
+//                 }
+//                 onMouseEnter={() => setIndex(i)}
+//               />
+//             ))}
+//           </div>
+//         </div>
+
+//         <div className="product-detail-desc">
+//           <h1>{name}</h1>
+//           <div className="reviews">
+//             <div>
+//               <AiFillStar />
+//               <AiFillStar />
+//               <AiFillStar />
+//               <AiFillStar />
+//               <AiOutlineStar />
+//             </div>
+//             <p>(20)</p>
+//           </div>
+//           <h4>Details: </h4>
+//           <p>{details}</p>
+//           <p className="price">${price}</p>
+//           <div className="quantity">
+//             <h3>Quantity:</h3>
+//             <p className="quantity-desc">
+//               <span className="minus" onclick={decQty}>
+//                 <AiOutlineMinus />
+//               </span>
+//               <span className="num" onclick="">
+//                 {qty}
+//               </span>
+//               <span className="plus" onclick={incQty}>
+//                 <AiOutlinePlus />
+//               </span>
+//             </p>
+//           </div>
+//           <div className="buttons">
+//             <button type="button" className="add-to-cart" onclick="">
+//               Add to Cart
+//             </button>
+//             <button type="button" className="buy-now" onclick="">
+//               Buy Now
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//       <div className="maylike-products-wrapper">
+//         <h2>You may also like</h2>
+//         <div className="marquee">
+//           <div className="maylike-products-container track">
+//             {products.map((item) => (
+//               <Product key={item._id} product={item} />
+//             ))}
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export const getStaticPaths = async () => {
+//   const query = `*[_type == "product"] {
+//     slug{
+//       current
+//     }
+//   }
+//   `;
+
+//   const products = await client.fetch(query);
+
+//   const paths = products.map((product) => ({
+//     params: {
+//       slug: product.slug.current,
+//     },
+//   }));
+
+//   return {
+//     paths,
+//     fallback: "blocking",
+//   };
+// };
+
+// export const getStaticProps = async ({ params: { slug } }) => {
+//   const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+
+//   const productsQuery = '*[_type == "product"]';
+//   const product = await client.fetch(query);
+
+//   const products = await client.fetch(productsQuery);
+
+//   return {
+//     props: { products, product },
+//   };
+// };
+
+// export default ProductDetails;
+
 import React, { useState } from "react";
 import {
   AiOutlineMinus,
@@ -8,14 +140,18 @@ import {
 
 import { client, urlFor } from "../../lib/client";
 import { Product } from "../../components";
-
 import { useStateContext } from "../../context/StateContext";
+
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
-
   const [index, setIndex] = useState(0);
+  const { decQty, incQty, qty, onAdd, setShowCart } = useStateContext();
 
-  const { decQty, incQty, qty } = useStateContext();
+  const handleBuyNow = () => {
+    onAdd(product, qty);
+
+    setShowCart(true);
+  };
 
   return (
     <div>
@@ -30,6 +166,7 @@ const ProductDetails = ({ product, products }) => {
           <div className="small-images-container">
             {image?.map((item, i) => (
               <img
+                key={i}
                 src={urlFor(item)}
                 className={
                   i === index ? "small-image selected-image" : "small-image"
@@ -58,27 +195,30 @@ const ProductDetails = ({ product, products }) => {
           <div className="quantity">
             <h3>Quantity:</h3>
             <p className="quantity-desc">
-              <span className="minus" onclick={decQty}>
+              <span className="minus" onClick={decQty}>
                 <AiOutlineMinus />
               </span>
-              <span className="num" onclick="">
-                {qty}
-              </span>
-              <span className="plus" onclick={incQty}>
+              <span className="num">{qty}</span>
+              <span className="plus" onClick={incQty}>
                 <AiOutlinePlus />
               </span>
             </p>
           </div>
           <div className="buttons">
-            <button type="button" className="add-to-cart" onclick="">
+            <button
+              type="button"
+              className="add-to-cart"
+              onClick={() => onAdd(product, qty)}
+            >
               Add to Cart
             </button>
-            <button type="button" className="buy-now" onclick="">
+            <button type="button" className="buy-now" onClick={handleBuyNow}>
               Buy Now
             </button>
           </div>
         </div>
       </div>
+
       <div className="maylike-products-wrapper">
         <h2>You may also like</h2>
         <div className="marquee">
@@ -95,7 +235,7 @@ const ProductDetails = ({ product, products }) => {
 
 export const getStaticPaths = async () => {
   const query = `*[_type == "product"] {
-    slug{
+    slug {
       current
     }
   }
@@ -117,11 +257,12 @@ export const getStaticPaths = async () => {
 
 export const getStaticProps = async ({ params: { slug } }) => {
   const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
-
   const productsQuery = '*[_type == "product"]';
-  const product = await client.fetch(query);
 
+  const product = await client.fetch(query);
   const products = await client.fetch(productsQuery);
+
+  console.log(product);
 
   return {
     props: { products, product },
