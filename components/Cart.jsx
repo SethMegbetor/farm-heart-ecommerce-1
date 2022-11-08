46a55c56f46072742346b4b113f560e3bc630ce9
“SethMegbetor” <sethmegbetor@gmail.com>
2022-11-08 19:39:11 +0000

    creating the jsx for the cart

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,15 +1,22 @@
-import React, { useRef } from 'react'
-import Link from "next/link"
-import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from "react-icons/ai"
-import { TiDelelteOutline } from "react-icons/ti"
-import { Toast } from "react-hot-toast"
-import { useStateContext } from "../context/StateContext"
- import { urlFor } from "../lib/client"
+import React, { useRef } from "react";
+import Link from "next/link";
+import {
+  AiOutlineMinus,
+  AiOutlinePlus,
+  AiOutlineLeft,
+  AiOutlineShopping,
+} from "react-icons/ai";
+import { TiDelelteOutline } from "react-icons/ti";
+import { Toast } from "react-hot-toast";
+import { useStateContext } from "../context/StateContext";
+import { urlFor } from "../lib/client";
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const cartRef = useRef();
+  const { totalPrice, totalQuantities, cartItems, setShowCart } =
+    useStateContext();
 
-export default Cart
+  return <div>Cart</div>;
+};
+
+export default Cart;
