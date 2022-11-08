e98083de332cfe2e8d4f3d5432df2aa821960131
“SethMegbetor” <sethmegbetor@gmail.com>
2022-11-08 19:37:08 +0000

    getting started with the Cart

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import Link from "next/link"
+import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from "react-icons/ai"
+import { TiDelelteOutline } from "react-icons/ti"
+import { Toast } from "react-hot-toast"
+import { useStateContext } from "../context/StateContext"
+ import { urlFor } from "../lib/client"
 
 const Cart = () => {
   return (
