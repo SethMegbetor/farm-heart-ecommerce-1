779c011fb50cdda497c71cd5a4887f3446e74493
“SethMegbetor” <sethmegbetor@gmail.com>
2022-11-09 23:28:32 +0000

    refactoring

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,3 @@
-// import React from "react";
-
-// import { Layout } from "../components";
-// import { StateContext } from "../context/StateContext";
-// import { Toaster } from "react-hot-toast";
-
-// import "../styles/globals.css";
-
-// function MyApp({ Component, pageProps }) {
-//   return (
-//     <StateContext>
-//       <Layout>
-//         <Toaster/>
-//         <Component {...pageProps} />
-//       </Layout>
-//     </StateContext>
-//   );
-// }
-
-// export default MyApp;
-
 import React from "react";
 import { Toaster } from "react-hot-toast";
 
