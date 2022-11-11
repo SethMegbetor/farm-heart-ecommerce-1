fc506869df97ae3e28c03a26f8ea4afe34421041
“SethMegbetor” <sethmegbetor@gmail.com>
2022-11-11 19:40:58 +0000

    creating payment method with try catch

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -1,3 +1,13 @@
-import Stripe from "stripe"
+import Stripe from "stripe";
 
-const stripe = new Stripe()
+const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      
+    } catch (error) {
+      res.status(500).json({ statusCode: 500, message: error.message });
+    }
+  }
+}
