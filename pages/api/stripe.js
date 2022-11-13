91ee1e792ebc7470382c67edbba779d69783e772
“SethMegbetor” <sethmegbetor@gmail.com>
2022-11-13 22:17:00 +0000

    done with the success page

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -113,11 +113,11 @@ export default async function handler(req, res) {
           };
         }),
 
-        success_url: `${req.headers.origin}/?success=true`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
+        // success_url: `${req.headers.origin}/?success=true`,
+        // cancel_url: `${req.headers.origin}/?canceled=true`,
 
-        // success_url: `${req.headers.origin}/success`,
-        // cancel_url: `${req.headers.origin}/canceled`,
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/canceled`,
       };
 
       // Create Checkout Sessions from body params.
