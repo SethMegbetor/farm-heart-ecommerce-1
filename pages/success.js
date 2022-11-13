3836dcf11a0a157982dc17c4c085a7b927c75101
“SethMegbetor” <sethmegbetor@gmail.com>
2022-11-13 21:51:30 +0000

    done with success page structure

--- pages/success.js
+++ pages/success.js
@@ -1,12 +1,39 @@
 import React, { useState, useEffect } from "react";
 import Link from "next/link";
 import { BsBagCheckFill } from "react-icons/bs";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 
 import { useStateContext } from "../context/StateContext";
 
 const Success = () => {
-  return <div>Success</div>;
+  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+
+  // const [order, setOrder] = useState(null);
+
+  
+
+  return (
+    <div className="success-wrapper">
+      <div className="success">
+        <p className="icon">
+          <BsBagCheckFill />
+        </p>
+        <h2>Thank you for your order!</h2>
+        <p className="email-msg">Check your email inbox for the receipt</p>
+        <p className="description">
+          If you have any questions, please email
+          <a href="mailto:sethmegbetor@gmail.com" className="email">
+            sethmegbetor@gmail.com
+          </a>
+        </p>
+        <Link href="/">
+          <button type="button" width="300px" className="btn">
+            continue Shopping
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Success;
