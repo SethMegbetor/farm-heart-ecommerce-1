a2a504b30a0df9acc144a7062672216214465ac8
“SethMegbetor” <sethmegbetor@gmail.com>
2022-11-13 21:41:13 +0000

    creating a success page

--- pages/success.js
+++ pages/success.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { BsBagCheckFill } from "react-icons/bs";
 import { useRouter } from "next/router";
 
-import { useStateContext } from "../";
+import { useStateContext } from "../context/StateContext";
 
 const Success = () => {
   return <div>Success</div>;
